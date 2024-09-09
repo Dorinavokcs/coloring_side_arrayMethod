e7bf24528621292d6cc4291446cf2300386bb10e
Dorinavokcs <l.cs.dorina@gmail.com>
2024-09-09 14:10:35 +0200

    update

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ const makeBoxes =() => {
         {number: 14},
         {number: 15}
     ];
-    const content = data.map()
+    const content = data.map(({number}) => `div id="${number}" class="box">${number}</div>`)
 }
 
 // A négyzetek megjelenítése a HTML-ben:
